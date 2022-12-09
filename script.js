5e81c5caa3dcd76df2deea74eae5e5e5a65fd2aa
Sylwia <g.sylwia@vp.pl>
2022-12-09 21:46:31 +0100

    feature: add validation to name and password input

--- script.js
+++ script.js
@@ -29,10 +29,25 @@ const checkForm = (input) => {
     })
 }
 
+const checkLength = (input, min) => {
+    if (input.value.length < min) {
+        showError(input, `${input.previousElementSibling.innerText.slice(0,-1)} składa się z min ${min} znaków.`)
+    }
+}
+
+const checkPassowrd = (pass1, pass2) => {
+    if (pass1.value !== pass2.value) {
+        showError(pass2, "Hasła do siebie nie pasują")
+    }
+}
+
 sendBtn.addEventListener('click', e => {
     e.preventDefault();
 
     checkForm([username, pass, pass2, email]);
+    checkLength(username, 3);
+    checkLength(pass, 8);
+    checkPassowrd(pass, pass2);
 })
 
 clearBtn.addEventListener('click', e => {
